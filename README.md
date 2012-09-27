f5bfed49b39849b486759fc2c4b5e329a7f6ba38
Adam Merrifield <macagp@gmail.com>
2012-09-27 15:34:47 -0400

    Updated to version 1.1.0, added option to update local files.

--- README.md
+++ README.md
@@ -1,18 +1,75 @@
 # RW resource switch
 
-This is a shell script that will be inserted into our themes that will allow users to switch a themes dependence from CDN files to local files and back again.
+This is a shell script slowly making it's way into our themes that will allow users to switch a themes dependence from CDN files (plus local fall-back) to local files only and back again. It also allows users to update their local script files to be current with the latest CDN files or revert back to the last saved local version.
 
-It would work something like this:
+## Converting to a local only theme
 
-1. A user drags their theme onto terminal, then appends the path with `switch.sh -l local`:
+There might be cases where having your site draw from CDN files might not be appropriate for your situation. Some examples include: 
+
+- your site is on an intranet with no outside access. 
+- your site is working well as it is.
+- you'd rather be in control of your own destiny.
+
+
+1. To convert to a local only theme, ***drag and drop*** your theme onto ***Terminal.app***, then append the path (removing the space first) with `switch.sh -l local`. For example, the following is what this would look like on my computer with the "alltr" theme:
 
 	```bash
 	MacBook-Pro:~/Library/Application\ Support/Themes/seydesign\ alltr.rwtheme/switch.sh -l local
 	```
+
+	Notice that there is no space between `...alltr.rwtheme/` and `switch.sh`.
+
 2. To switch back, the user does the same, but this time appends the name with `switch.sh -l cdn`:
 
 	```bash
 	MacBook-Pro:~/Library/Application\ Support/Themes/seydesign\ alltr.rwtheme/switch.sh -l cdn
 	```
 
-See the usage instructions within the script.
+	Again, notice that there is no space between `...alltr.rwtheme/` and `switch.sh`.
+
+## Updating local script files
+
+By serving some of a themes most frequently updated files "from the cloud" we offer continual updates and improvements to themes and sites without the disruption and headache of updating entire themes. If you've decided to internalize your script files then you lose out on these updates, so another feature of the `switch.sh` script allows you to update your local script files to match that of the most current CDN files.
+
+It doesn't matter whether you are using local only or CDN plus local fall-back, this feature will update your local files either way.
+
+1. To update your local script files, ***drag and drop*** your theme onto ***Terminal.app***, then append the path (removing the space first) with `switch.sh -u update`. For example, the following is what this would look like on my computer with the "alltr" theme:
+
+	```bash
+	MacBook-Pro:~/Library/Application\ Support/Themes/seydesign\ alltr.rwtheme/switch.sh -u update
+	```
+
+	And again (I can't stress this enough), notice that there is no space between `...alltr.rwtheme/` and `switch.sh`.
+
+2. If you're not happy with the updates or if something breaks, you can revert to the last saved backup copies with `switch.sh -u revert`:
+
+	```bash
+	MacBook-Pro:~/Library/Application\ Support/Themes/seydesign\ alltr.rwtheme/switch.sh -u revert
+	```
+
+	Eh-hem... once again, notice that there is no space between `...alltr.rwtheme/` and `switch.sh`.
+
+## All the commands at a glance
+
+Here some of the possible commands:
+
+```bash
+\# switch to local
+switch.sh -l local
+
+\# switch to cdn
+switch.sh -l cdn
+
+\# update local files
+switch.sh -u update
+
+\# revert to last save
+switch.sh -u revert
+
+\# check switch.sh version 
+switch.sh -V
+
+\# check switch.sh release date
+switch.sh -D
+```
+
--- switch.sh
+++ switch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-SWITCHVERSION=1.0.0
-DATE="September 25, 2012"
+SWITCHVERSION=1.1.0
+DATE="September 27, 2012"
 
 #############
 # FUNCTIONS #
@@ -19,7 +19,7 @@ switch $SWITCHVERSION ($DATE).
 
 Usage:
 
-	switch.sh [-options] [-h] [-l <local/cdn>] [-V] [-D]
+	switch.sh [-options] [-h] [-l <local/cdn>] [-u <update/revert>] [-V] [-D]
 
 The default action is to switch a theme from using CDN files with local fallback to an all local system.
 
@@ -27,6 +27,7 @@ The default action is to switch a theme from using CDN files with local fallback
 OPTIONS:
 	-h	Show this message
 	-l	location (local or cdn)
+	-u	update (update or revert)
 	-V	show the version information
 	-D	show date of version
 	
@@ -39,6 +40,12 @@ EXAMPLES:
 	switch.sh -l cdn
 		This will convert your theme into one that references CDN files with local files used as a fallback.
 
+	switch.sh -u update
+		This will update you local script files with the code from the current CDN versions.
+
+	switch.sh -u revert
+		This will revert you local script files to the last version stored.
+
 EOF1
 }
 
@@ -48,8 +55,8 @@ EOF1
 
 local_fn() {
 # copy original
-if [[ ! -f "$NPATH"/Contents/template/"$THEMENAME"/.index.cdn ]]; then
-	cp "$NPATH"/Contents/template/"$THEMENAME"/index.html "$NPATH"/Contents/template/"$THEMENAME"/.index.cdn
+if [[ ! -f "$HTMLPATH"/.index.cdn ]]; then
+	cp "$HTMLPATH"/index.html "$HTMLPATH"/.index.cdn
 
 	# modify the script lines
 	sed -n '
@@ -67,10 +74,10 @@ if [[ ! -f "$NPATH"/Contents/template/"$THEMENAME"/.index.cdn ]]; then
 	        # print
 	        p
 	}
-	' "$NPATH"/Contents/template/"$THEMENAME"/index.html > "$NPATH"/Contents/template/"$THEMENAME"/temp.html;
+	' "$HTMLPATH"/index.html > "$HTMLPATH"/temp.html;
 
 	# replace the original
-	mv "$NPATH"/Contents/template/"$THEMENAME"/temp.html "$NPATH"/Contents/template/"$THEMENAME"/index.html
+	mv "$HTMLPATH"/temp.html "$HTMLPATH"/index.html
 
 	# message
 	echo "SUCCESS! Your ${THEMENAME} theme has been successfully internalized."
@@ -90,8 +97,8 @@ fi
 cdn_fn() {
 
 # if backup file exists
-if [[ -f "$NPATH"/Contents/template/"$THEMENAME"/.index.cdn ]]; then
-	mv "$NPATH"/Contents/template/"$THEMENAME"/.index.cdn "$NPATH"/Contents/template/"$THEMENAME"/index.html
+if [[ -f "$HTMLPATH"/.index.cdn ]]; then
+	mv "$HTMLPATH"/.index.cdn "$HTMLPATH"/index.html
 	# message
 	echo "SUCCESS! Your ${THEMENAME} theme now uses CDN files."
 	sleep 1
@@ -103,42 +110,119 @@ fi
 
 }
 
+#////////#
+# UPDATE #
+#////////#
+
+update_fn() {
+
+# UPDATE COMMON.LOCAL.JS #
+
+# backup current common.local.js
+cp "$SCRIPTPATH"/common.local.js "$SCRIPTPATH"/.common.local.cdn
+
+# get common.min.js
+curl https://d2c8zg9eqwmdau.cloudfront.net/rw/common.min.js > "$SCRIPTPATH"/common.local.js
+
+# get theme.min.js
+echo -e "\n\n/*\n\tTheme scripts\n*/\n" >> "$SCRIPTPATH"/common.local.js
+curl https://"$CDN".cloudfront.net/rw/"$THEMENAME".min.js >> "$SCRIPTPATH"/common.local.js
+
+# add html5shiv
+echo -e "\n" >> "$SCRIPTPATH"/common.local.js
+curl https://html5shiv.googlecode.com/svn/trunk/html5.js >> "$SCRIPTPATH"/common.local.js
+
+# UPDATE PRETTYPHOTO FILES ONLY #
+
+#backup jquery.prettyphoto.css
+cp "$PRETTYPHOTOPATH"/jquery.prettyphoto.css "$PRETTYPHOTOPATH"/.jquery.prettyphoto.css.cdn
+
+#backup fresh jquery.prettyphoto.css
+curl https://d2c8zg9eqwmdau.cloudfront.net/prettyphoto/jquery.prettyPhoto.css > "$PRETTYPHOTOPATH"/jquery.prettyPhoto.css
+
+#backup jquery.prettyphoto.js
+cp "$PRETTYPHOTOPATH"/jquery.prettyphoto.js "$PRETTYPHOTOPATH"/.jquery.prettyphoto.js.cdn
+
+#backup fresh jquery.prettyphoto.js
+curl https://d2c8zg9eqwmdau.cloudfront.net/prettyphoto/jquery.prettyPhoto.js > "$PRETTYPHOTOPATH"/jquery.prettyPhoto.js
+
+# message
+echo "SUCCESS! The local script files for your ${THEMENAME} theme have been successfully updated."
+sleep 1 
+
+}
+
+#////////#
+# UPDATE #
+#////////#
+
+revert_fn() {
+if [[ -f "$SCRIPTPATH"/.common.local.cdn ]]; then
+	mv "$SCRIPTPATH"/.common.local.cdn "$SCRIPTPATH"/common.local.js
+	mv "$PRETTYPHOTOPATH"/.jquery.prettyphoto.js.cdn "$PRETTYPHOTOPATH"/jquery.prettyphoto.js
+	mv "$PRETTYPHOTOPATH"/.jquery.prettyphoto.css.cdn "$PRETTYPHOTOPATH"/jquery.prettyphoto.css
+	
+	# message
+	echo "SUCCESS! The local script files for your ${THEMENAME} theme have been reverted to the last backup version."
+	sleep 1 
+else
+	# message
+	echo "WARNING! There are no backup versions to revert to."
+	sleep 1 
+fi
+}
+
+
 #############
 # VARIABLES #
 #############
 
 NPATH="$(dirname "$0")"
-THEMENAME=`ls "$NPATH"/Contents/template/`
+if [[ -d "$NPATH"/Contents/template ]]; then
+	THEMENAME=`ls "$NPATH"/Contents/template/`
+	SCRIPTPATH="$NPATH"/Contents/template/"$THEMENAME"
+	HTMLPATH="$NPATH"/Contents/template/"$THEMENAME"
+	PRETTYPHOTOPATH="$NPATH"/Contents/options/prettyphoto
+else
+	SCRIPTPATH="$NPATH"/Contents/scripts
+	HTMLPATH="$NPATH"/Contents
+	PRETTYPHOTOPATH="$NPATH"/Contents/css/prettyphoto
+fi
+if [[ ! -f "$HTMLPATH"/.index.cdn ]]; then
+	THISINDEX="$HTMLPATH"/index.html
+else
+	THISINDEX="$HTMLPATH"/.index.cdn
+fi
+THEMENAME=`grep -v "common" "$THISINDEX" | grep -F "cloudfront.net/rw/" | sed -e 's#.*cloudfront\.net/rw/\(.*\)\.min\.js.*#\1#'`
+CDN=`grep -v "$THEMENAME" "$THISINDEX" | grep -F "cloudfront.net/rw/" | sed -e 's#.*https://\(.*\)\.cloudfront.*#\1#'`
 LOCATION=
+UPDATE=
 
 # get arguments
-while getopts ":hl:V:D" OPTION
+while getopts ":hl:u:VD" OPTION
 do
-	 case $OPTION in
-		 h) usage_fn; exit 1;;
-		 l) LOCATION=${OPTARG};;
-		 V) echo $SWITCHVERSION; exit 1;;
-		 D) echo $DATE; exit 1;;
-		 ?) usage_fn; exit 1;;
-	 esac
+	case $OPTION in
+		h) usage_fn; exit 1;;
+		l) LOCATION=${OPTARG};;
+		u) UPDATE=${OPTARG};;
+		V) echo $SWITCHVERSION; exit 1;;
+		D) echo $DATE; exit 1;;
+		?) usage_fn; exit 1;;
+	esac
 done
 
-# test for required arguments
-if [[ -z $LOCATION ]]; then
-	 usage_fn
-	 exit 1
+# test location arg
+if [[ $LOCATION == local ]]; then
+	local_fn
+elif [[ $LOCATION == cdn ]]; then
+	cdn_fn
+fi
 
-else 
-	if [[ $LOCATION == local ]]; then
-		local_fn
-		exit 1
-	elif [[ $LOCATION == cdn ]]; then
-		cdn_fn
-		exit 1
-	else
-		usage_fn
-		exit 1
-	fi
+# test update arg
+if [[ $UPDATE == update ]]; then
+	update_fn
+elif [[ $UPDATE == revert ]]; then
+	revert_fn
 fi
 
 exit 0
